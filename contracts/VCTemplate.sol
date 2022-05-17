69ede43e968efc0938259efe2d9f2d595398e88c
viac92 <64146594+viac92@users.noreply.github.com>
2022-05-17 18:00:22 +0200

    removing not necessary import

--- contracts/VCTemplate.sol
+++ contracts/VCTemplate.sol
@@ -3,12 +3,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract VCTemplate is Ownable, ReentrancyGuard {
+contract VCTemplate is ReentrancyGuard {
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
   
