24e8a374e45895b6dc665c5fa2ac5d493211099f
viac92 <64146594+viac92@users.noreply.github.com>
2022-05-17 17:23:57 +0200

    set vcwallet

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
-const DataGen = artifacts.require(".DataGen.sol");
-const VCTemplate = artifacts.require(".VCTemplate.sol");
+const DataGen = artifacts.require("./DataGen.sol");
+const VCTemplate = artifacts.require("./VCTemplate.sol");
 
-const vcWallet = "";
+const vcWallet = "0xF9d431E1dd5088E1Ee252A2B9250363f07e34AA0";
 
 module.exports = async function (deployer) {
   await deployer.deploy(DataGen);
