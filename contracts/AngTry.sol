d0e311be9f8c8b989512a59f8e52d1d621b770d1
viac92 <64146594+viac92@users.noreply.github.com>
2022-06-22 14:32:29 +0200

    add AngTry

---
+++ contracts/AngTry.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract AngTry is ReentrancyGuard {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+  
+  /* Total tokens amount */
+  uint256 public constant amount = 100 * (10**18);
+  /* Release start time */
+  uint256 public constant releaseStart = 1655906400;
+  /* the address of the token contract */
+  IERC20 public dataGen;
+  /* the vc wallet where distribute the #DG */
+  address public vcWallet; 
+
+
+  /*  initialization, set the token and vc addresses */
+  constructor(IERC20 _dataGen, address _vcWallet) {
+    dataGen = _dataGen;
+    vcWallet = _vcWallet;
+  }
+
+  event SetVcWalletAddress(address indexed user, address indexed vcWallet); 
+
+  modifier firstRelease() {
+    require(block.timestamp >= releaseStart, "Release is still locked.");
+    _;
+  }
+
+  modifier VcWalletOwner() {
+    require(msg.sender == vcWallet, "You are not the Vc owner");
+    _;
+  }
+
+  function releaseDataGen() public firstRelease nonReentrant {
+    require(dataGen.balanceOf(address(this)) > 0, "Zero #DG left.");
+
+      uint256 epochs = block.timestamp.sub(releaseStart).div(3600).add(1);
+      if (epochs > 24) epochs = 24;
+
+      uint256 balance = dataGen.balanceOf(address(this));
+      uint256 leftAmount = amount.sub(amount.mul(epochs).div(24));
+
+      require(balance > leftAmount, "Already released.");
+      uint256 transferAmount = balance.sub(leftAmount);
+      if(transferAmount > 0) {
+        dataGen.safeTransfer(vcWallet, transferAmount);
+    }
+	}
+
+  /* vc wallet update, only owner of the vc wallet can do it. */
+  function setVcWallet(address _vcWallet) public VcWalletOwner {
+    vcWallet = _vcWallet;
+    emit SetVcWalletAddress(msg.sender, _vcWallet);
+  }
+
+  function checkFunds() public view returns (uint256) {
+		return dataGen.balanceOf(address(this));
+	}
+}
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,7 @@
 const DataGen = artifacts.require("./DataGen.sol");
 const VCTemplate = artifacts.require("./VCTemplate.sol");
 const TVentures = artifacts.require("./T_Ventures_VC.sol");
+const AngTry = artifacts.require("./AngTry.sol");
 
 const vcWallet = "0xF9d431E1dd5088E1Ee252A2B9250363f07e34AA0";
 
@@ -8,4 +9,5 @@ module.exports = async function (deployer) {
   await deployer.deploy(DataGen);
   await deployer.deploy(VCTemplate, DataGen.address, vcWallet);
   await deployer.deploy(TVentures, DataGen.address, vcWallet);
+  await deployer.deploy(AngTry, DataGen.address, vcWallet);
 };
